22f450cc34ddbd757920d9669965632e19b92f2d
thieryw <williamalexthiery@hotmail.fr>
2022-04-11 20:39:01 +0200

    write todo created, todoCompletedToggled and thunks

--- src/core/usecases/manageTodos.ts
+++ src/core/usecases/manageTodos.ts
@@ -5,6 +5,7 @@ import type { State } from "../setup";
 import { createSelector } from "@reduxjs/toolkit";
 import type { Todo } from "../ports/TodoClient";
 import { createObjectThatThrowsIfAccessed } from "core/tools/createObjectThatThrowsIfAccessed";
+import { flip } from "tsafe/flip";
 
 export type ManageTodosState = {
 	todos: Todo[];
@@ -18,12 +19,26 @@ export const { reducer, actions, name } = createSlice({
 			const { todos } = payload;
 			return { todos };
 		},
-		"todoDeleted": (state, { payload }: PayloadAction<{ todoId: number; }>) => {
+		"todoDeleted": (state, { payload }: PayloadAction<{ todoId: string; }>) => {
 			const { todoId } = payload;
 
 			state.todos = state.todos.filter(todo => todo.id !== todoId);
 
 		},
+		"todoCreated": (state, { payload }: PayloadAction<{ todo: Todo }>) => {
+			const { todo } = payload;
+			state.todos.push({
+				...todo
+			})
+		},
+		"todoCompletedToggled": (state, { payload }: PayloadAction<{ todoId: string }>) => {
+			const { todoId } = payload;
+
+			const index = state.todos.findIndex(({ id }) => todoId === id);
+
+			flip(state.todos[index], "isCompleted")
+
+		}
 	},
 });
 
@@ -43,7 +58,7 @@ export const privateThunks = {
 
 export const thunks = {
 	"deleteTodo":
-		(params: { todoId: number; }): ThunkAction =>
+		(params: { todoId: string; }): ThunkAction =>
 			async (...args) => {
 
 				const { todoId } = params;
@@ -53,9 +68,34 @@ export const thunks = {
 				dispatch(actions.todoDeleted({ todoId }));
 
 			},
-	"getMaxTodo":
+	"createTodo":
+		(params: { message: string; }): ThunkAction =>
+			async (...args) => {
+				const { message } = params;
+				const [dispatch, , { todoClient }] = args;
+				await todoClient.createTodo({
+					message
+				});
+
+				todoClient.getTodos().then(todos => {
+					dispatch(actions.todoCreated({ "todo": todos[0] }))
+				});
+
+			},
+	"toggleTodoIsCompleted":
+		(params: { todoId: string; }): ThunkAction =>
+			async (...args) => {
+
+				const { todoId } = params;
+				const [dispatch, , { todoClient }] = args;
+
+				await todoClient.toggleTodoCompleted({ "id": todoId });
+
+				dispatch(actions.todoCompletedToggled({ todoId }))
+			}
+	/*"getMaxTodo":
 		(): ThunkAction<number> =>
-			 (...args) => {
+			(...args) => {
 				const [ dispatch ,getState,{ createStoreParams }]= args;
 
 				await dispatch(thunks.deleteTodo({ "todoId": 1 }));
@@ -63,14 +103,14 @@ export const thunks = {
 				const { todos } = getState().manageTodos;
 
 				return createStoreParams.maxTodo;
-			}
-	
+			}*/
+
 
 };
 
 export const selectors = (() => {
 	const todoCount = (state: State) => {
-		const { todos} = state.manageTodos;
+		const { todos } = state.manageTodos;
 		return todos.length;
 	};
 
@@ -80,9 +120,9 @@ export const selectors = (() => {
 	};
 
 	const percentageOfTodosCompleted = createSelector(
-		todoCount, 
-		completedTodoCount, 
-		(todoCount, completedTodoCount) => (todoCount/completedTodoCount)*100
+		todoCount,
+		completedTodoCount,
+		(todoCount, completedTodoCount) => (todoCount / completedTodoCount) * 100
 	);
 
 	return {
