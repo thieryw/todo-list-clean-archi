f3832480d15097c48de1b0f4d0c2adbe37a3d754
thieryw <williamalexthiery@hotmail.fr>
2022-05-07 17:33:46 +0200

    change id type to number and createTaskFlipBolleanValue

--- src/core/ports/TodoClient.ts
+++ src/core/ports/TodoClient.ts
@@ -1,14 +1,31 @@
 
 export type Todo = {
-	id: string;
+	id: number;
 	message: string;
 	isCompleted: boolean;
+	isSelected: boolean;
 };
 
 export type TodoClient = {
 	getTodos: () => Promise<Todo[]>;
 	createTodo: (params: { message: string; }) => Promise<Todo>;
-	deleteTodo: (params: { id: string; }) => Promise<void>;
-	toggleTodoCompleted: (params: { id: string }) => Promise<void>;
+	deleteTodo: (params: { id: number; }) => Promise<void>;
+	toggleTodoCompleted: (params: { id: number }) => Promise<void>;
+	toggleTodoSelected: (params: {id: number}) => Promise<void>;
 };
 
+export function createTaskFlipBooleanValue<
+	T extends {
+		id: number;
+		valueToFlip: {
+			[Key in keyof Todo]: Todo[Key] extends boolean ? Key : never
+		}[keyof Todo]
+	}
+>(params: {
+	action: (params: T)=> Promise<void>;
+}): {taskFlipBooleanValue: (params: T) => void} {
+	const { action: taskFlipBooleanValue } = params;
+
+	return { taskFlipBooleanValue };
+
+};
