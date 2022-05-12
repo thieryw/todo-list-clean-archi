b34ed72567e936556a2b95a2d51e8471bcd43a44
thieryw <williamalexthiery@hotmail.fr>
2022-05-13 01:27:55 +0200

    Task: attempt loading feedback, not to convincing for the moment

--- src/ui/Task.tsx
+++ src/ui/Task.tsx
@@ -1,43 +1,64 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import type { Task as TaskType } from "../core/ports/TodoListClient";
 import { useThunks } from "ui/coreApi";
 import { makeStyles } from "../theme";
 import { useConstCallback } from "powerhooks/useConstCallback";
-//import * as runEx from "run-exclusive";
 
+async function loader<T>(params: {
+	setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+	callBack: (params: T) => Promise<void>;
+	callBackParams: T
+}) {
+	const { callBack, setIsLoading, callBackParams } = params;
+	setIsLoading(true);
+	await callBack(callBackParams);
+	setIsLoading(false);
+}
 
 export const Task = memo((props: TaskType) => {
 	const { id, isCompleted, message, isSelected } = props;
-	const { manageTasksThunks } = useThunks();
+	const [isLoading, setIsLoading] = useState(false);
+	const { toggleTaskCompleted, toggleTaskSelected, deleteTask  } = useThunks().manageTasksThunks;
 
-	const toggleIsCompleted = useConstCallback((e: React.MouseEvent<HTMLInputElement, MouseEvent>)=>{
-			e.stopPropagation();
-			manageTasksThunks.toggleTaskCompleted({ id })
-	});
-
-	const toggleIsTaskSelected = useConstCallback(()=>{
-		manageTasksThunks.toggleTaskSelected({id});
-	});
-
-	const deleteTask = useConstCallback((e: React.MouseEvent<HTMLHeadingElement, MouseEvent>) => {
+	const toggleIsCompleted = useConstCallback((e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
 		e.stopPropagation();
-		manageTasksThunks.deleteTask({ id });
+		loader({
+			setIsLoading,
+			"callBack": toggleTaskCompleted,
+			"callBackParams": {id}
+		})
+	});
+
+	const toggleIsTaskSelected = useConstCallback(() => {
+		loader({
+			"callBack": toggleTaskSelected,
+			setIsLoading,
+			"callBackParams": { id }
+		})
+	});
+
+	const handleDelete = useConstCallback((e: React.MouseEvent<HTMLHeadingElement, MouseEvent>) => {
+		e.stopPropagation();
+		setIsLoading(true);
+		deleteTask({ id });
 	});
 
 	const { classes } = useStyles({
 		isCompleted,
-		isSelected
+		isSelected,
+		isLoading
 	});
+
 	return <div onClick={toggleIsTaskSelected} className={classes.root}>
-		<input readOnly={true} onClick={toggleIsCompleted} checked={isCompleted} type="checkbox" />
-		<p className={classes.message}>{message}</p>
-		<h2 className={classes.deleteButton} onClick={deleteTask}>X</h2>
+		<input className={classes.checkBox} readOnly={true} onClick={toggleIsCompleted} checked={isCompleted} type="checkbox" />
+		<p className={classes.message}>{isLoading ? "Loading" : message}</p>
+		<h2 className={classes.deleteButton} onClick={handleDelete}>X</h2>
 	</div>
 });
 
 
-const useStyles = makeStyles<{ isCompleted: boolean; isSelected: boolean }>()(
-	(...[, { isCompleted, isSelected }]) => ({
+const useStyles = makeStyles<{ isCompleted: boolean; isSelected: boolean; isLoading: boolean }>()(
+	(...[, { isCompleted, isSelected, isLoading }]) => ({
 		"root": {
 			"width": 400,
 			"height": 50,
@@ -46,17 +67,19 @@ const useStyles = makeStyles<{ isCompleted: boolean; isSelected: boolean }>()(
 			"justifyContent": "space-between",
 			"alignItems": "center",
 			"padding": "0px 10px 0px 10px",
-			"backgroundColor": isSelected ? "black" : undefined
+			"backgroundColor": isSelected ? "black" : undefined,
+			"pointerEvents": isLoading ? "none" : undefined
 
 		},
 		"message": {
-			"textDecoration": isCompleted ? "line-through" : undefined,
+			"textDecoration": isCompleted && !isLoading ? "line-through" : undefined,
 			"color": isSelected ? "white" : undefined
 		},
 		"deleteButton": {
 			"cursor": "pointer",
 			"color": isSelected ? "white" : undefined
-
+		},
+		"checkBox": {
 		}
 	}))
 
